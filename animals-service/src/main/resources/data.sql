ae2f54ca957e12dccbeefda6411e4b2996ccca94
Renan <renan.bete@gmail.com>
2024-01-25 10:22:31 -0300

    Adicionado mais entradar no data.sql

--- animals-service/src/main/resources/data.sql
+++ animals-service/src/main/resources/data.sql
@@ -1,10 +1,16 @@
 INSERT INTO animal (especie, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, id_recebedor, nome_recebedor, porte, funcionario_id)
-VALUES ('Cachorro', 'Rex', 2, 'Vira-lata', '2022-01-01', 'Encontrado na rua', 1, 'João', 'Médio',1);
+VALUES ('Cachorro', 'Amora', 8, 'Perdigueiro', '2022-01-01', 'Abaixo do peso e com carrapatos', 1, 'Renan', 'Médio',1);
 
--- Exemplo 2: Animal adotado por um funcionário
+
 INSERT INTO animal (especie, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, id_recebedor, nome_recebedor, porte, data_adocao, funcionario_id)
-VALUES ('Gato', 'Whiskers', 1, 'Siamês', '2022-02-01', 'Resgatado de um abrigo', 2, 'Maria', 'Pequeno', '2022-03-01', 2);
+VALUES ('Gato', 'Lua', 17, 'Tombinha', '2022-02-01', 'Resgatado de um abrigo', 2, 'Tais', 'Pequeno', '2022-03-01', 2);
 
--- Exemplo 3: Animal falecido
+
 INSERT INTO animal (especie, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, id_recebedor, nome_recebedor, porte, data_obito, funcionario_id)
-VALUES ('Cachorro', 'Buddy', 4, 'Labrador', '2022-03-01', 'Entregue pelo dono', 1, 'João', 'Grande', '2022-04-15',1);
+VALUES ('Cachorro', 'Megg', 4, 'Poodle', '2022-03-01', 'Sarnenta', 1, 'Renan', 'Pequeno', '2022-04-15',1);
+
+INSERT INTO animal
+
+   (especie, nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, id_recebedor, nome_recebedor, porte, data_obito, funcionario_id)
+VALUES
+    ('Gato', 'Estrela', 8, 'English shorthair', NOW(), 'Obesa', 2, 'Tais', 'Medio', 2);
